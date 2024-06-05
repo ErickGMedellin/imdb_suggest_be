10ef64b1500669e89d4f78c7d68bbf4c7361c6ba
erickgmedellin <erick.gonzalez@bluestart.mx>
2024-06-05 16:39:20 -0600

    Two more filter endpoints added.

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import save_csv, RandomDataView
+from .views import save_csv, RandomDataView, QueryByRatingView, QueryByGenre
 
 urlpatterns = [
-    path("save_csv", save_csv, name="save_csv"),
-    path('three_random', RandomDataView.as_view(), name='random-data')
+    path("save_csv/", save_csv, name="save_csv"),
+    path('three_random/', RandomDataView.as_view(), name='random-data'),
+    path('rating/', QueryByRatingView.as_view(), name= 'query-by-rating'),
+    path('genre/', QueryByGenre.as_view(), name='query-by-genre')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib import messages
-from io import StringIO
 from django.db import transaction
 import pandas as pd
 from movies.models import Movie, Genre, GenreToken
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.request import Request
 from .serializers import MovieSerializer
 from random import sample
 
@@ -55,16 +53,16 @@ def save_csv(request):
                     # Saving genres:
                     for genre_name in genres:
                         genre_name = genre_name.strip()
-                        genre, created = Genre.objects.get_or_create(name= genre_name)
-                        GenreToken.objects.get_or_create(movie=movie, genre = genre)
+                        genre, created = Genre.objects.get_or_create(name=genre_name)
+                        GenreToken.objects.get_or_create(movie=movie, genre=genre)
 
-            return JsonResponse({"message": "CSV file successfully uploaded and processed"}, status = 200)
+            return JsonResponse({"message": "CSV file successfully uploaded and processed"}, status=200)
 
         except Exception as e:
 
             print(f"Error: {e}")
 
-            return JsonResponse({"message": "There's been an error saving data."}, status= 500)
+            return JsonResponse({"message": "There's been an error saving data."}, status=500)
         
 class RandomDataView(generics.GenericAPIView):
     serializer_class = MovieSerializer
@@ -72,8 +70,45 @@ class RandomDataView(generics.GenericAPIView):
     @csrf_exempt
     def get(self, request, *args, **kwargs):
         total_count = Movie.objects.count()
-        random_ids = sample(range(1,total_count+1), min(3, total_count))
-        random_data = Movie.objects.filter(id__in = random_ids)
+        random_ids = sample(range(1, total_count+1), min(3, total_count))
+        random_data = Movie.objects.filter(id__in=random_ids)
         serializer = self.get_serializer(random_data, many=True)
 
         return Response(serializer.data)
+    
+class QueryByRatingView(generics.GenericAPIView):
+    serializer_class = MovieSerializer
+
+    @csrf_exempt
+    def get(self,request,**kwargs):
+        min_value = request.data.get('min')
+        max_value = request.data.get('max')
+
+        parsed_min = min_value if min_value else 0
+        parsed_max = max_value if max_value else 10
+
+        movies_filtered = Movie.objects.filter(avgRating__gte=parsed_min, avgRating__lte=parsed_max)
+        random_movies = movies_filtered.order_by('?')[:min(3, movies_filtered.count())]
+
+        serializer = self.get_serializer(random_movies, many=True)
+
+        return Response(serializer.data)
+
+
+class QueryByGenre(generics.GenericAPIView):
+    serializer_class = MovieSerializer
+
+    @csrf_exempt
+    def get(self, request, **kwargs):
+
+        genreRequestList = request.data.get('genres', [])
+
+        genreListName = Genre.objects.filter(name__in=genreRequestList)
+        tokenId = GenreToken.objects.filter(genre__in=genreListName).values_list('movie_id', flat=True).distinct()
+        movies_filtered = Movie.objects.filter(id__in=tokenId)
+        movies = movies_filtered.order_by('?')[:min(3, movies_filtered.count())]
+
+        serializer = self.get_serializer(movies, many=True)
+
+        return Response(serializer.data)
+        
