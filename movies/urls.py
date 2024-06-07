b4e3283cf026f9b4cacc80339999c85b1278e394
erickgmedellin <erick.gonzalez@bluestart.mx>
2024-06-06 18:24:53 -0600

    Final details for general query endpoint.

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
-from .views import save_csv, RandomDataView, QueryByRatingView, QueryByGenre
+from .views import save_csv, RandomDataView, \
+            QueryByRatingView, QueryByGenre, GeneralQuery
 
 urlpatterns = [
     path("save_csv/", save_csv, name="save_csv"),
     path('three_random/', RandomDataView.as_view(), name='random-data'),
-    path('rating/', QueryByRatingView.as_view(), name= 'query-by-rating'),
-    path('genre/', QueryByGenre.as_view(), name='query-by-genre')
+    path('rating/', QueryByRatingView.as_view(), name='query-by-rating'),
+    path('genre/', QueryByGenre.as_view(), name='query-by-genre'),
+    path('query/', GeneralQuery.as_view(), name='general-query'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.db import transaction
 import pandas as pd
 from movies.models import Movie, Genre, GenreToken
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from .serializers import MovieSerializer
@@ -14,6 +14,7 @@ from random import sample
 def index(request):
     return HttpResponse("Hello, world. You're at the movies index.")
 
+# This save_csv function is used at the save_csv endpoint to save information into mysql database.
 @csrf_exempt
 def save_csv(request):
     
@@ -64,6 +65,7 @@ def save_csv(request):
 
             return JsonResponse({"message": "There's been an error saving data."}, status=500)
         
+#   This function was a first aproximation to the queries procedure to ger 3 random values.
 class RandomDataView(generics.GenericAPIView):
     serializer_class = MovieSerializer
 
@@ -76,11 +78,12 @@ class RandomDataView(generics.GenericAPIView):
 
         return Response(serializer.data)
     
+#   This is another query using a rating range
 class QueryByRatingView(generics.GenericAPIView):
     serializer_class = MovieSerializer
 
     @csrf_exempt
-    def get(self,request,**kwargs):
+    def get(self,request):
         min_value = request.data.get('min')
         max_value = request.data.get('max')
 
@@ -94,12 +97,12 @@ class QueryByRatingView(generics.GenericAPIView):
 
         return Response(serializer.data)
 
-
+#   This function returns 3 random values queried by Genre
 class QueryByGenre(generics.GenericAPIView):
     serializer_class = MovieSerializer
 
     @csrf_exempt
-    def get(self, request, **kwargs):
+    def get(self, request):
 
         genreRequestList = request.data.get('genres', [])
 
@@ -112,3 +115,45 @@ class QueryByGenre(generics.GenericAPIView):
 
         return Response(serializer.data)
         
+#   This is the final function, the Front End project will make this request every time.
+#   This function considers diferent filters and the cases where some of them are missing.
+class GeneralQuery(generics.GenericAPIView):
+    serializer_class= MovieSerializer
+
+    @csrf_exempt
+    def get(self, request,):
+
+        #   First we get the request information. If some data are missing 
+        # we used the ternary operator to give a default value.
+        year_min = request.data.get('year_min') if request.data.get('year_min') else 1950
+        year_max = request.data.get('year_max') if request.data.get('year_max') else 2024
+        rate_min = request.data.get('rate_min') if request.data.get('rate_min') else 6.8
+        rate_max = request.data.get('rate_max') if request.data.get('rate_max') else 9
+        requestGenres = request.data.get('genres', [])
+        requestTypes = request.data.get('type', []) 
+
+        #   First filter: using the default values to reduce the length of the total data.
+        movies_filtered = Movie.objects.filter(avgRating__gte=rate_min, 
+                                                      avgRating__lte=rate_max,
+                                                      year__gte=year_min, 
+                                                      year__lte=year_max)
+        #   Second filter: it's done if the request has any 'type' to filter with.
+        if ( len(requestTypes) > 0):
+            movies_filtered = movies_filtered.filter(type__in=requestTypes)
+
+        #   Third filter: if the request has any 'genre', we filter the 
+        # genre data and then the movies_filtered using the genres id.
+        if( len(requestGenres)>0):
+            genreListName = Genre.objects.filter(name__in=requestGenres)
+            tokenId = GenreToken.objects.filter(genre__in=genreListName).values_list('movie_id', flat=True).distinct()
+            movies_filtered = movies_filtered.filter(id__in=tokenId)
+
+        movies = movies_filtered.order_by('?')[:min(3, movies_filtered.count())]
+
+        serializer = self.get_serializer(movies, many=True)
+
+        if(len(serializer.data)== 0):
+            return Response("There's no matches for that search.", status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer,status=status.HTTP_200_OK)
+
